4cafa0710673e7c17608f203632d496817fe4e18
BRusinov <borislav.rusinov@gmail.com>
2016-03-10 00:05:07 +0200

    Modifying script javascript. Adding requests

--- course_work/src/main/webapp/js/script.js
+++ course_work/src/main/webapp/js/script.js
@@ -1,14 +1,74 @@
 $(document).ready(function() {
     "use strict";
+    var PLAYER_ENDPOINT = "http://localhost:3000/Regular_user";
+	function playerEndpoint(playerId) {
+		return PLAYER_ENDPOINT + "/" + playerId;
+	}
+	var all;
+    function listComments() {
+        return $.ajax(playerEndpoint(1), {
+            method: "GET",
+            dataType: "json"
+        });
+    }
+    
+    function reloadComments() {
+        listComments().then(function(response) {
+            function addComment(player) {
+            	$("#new_ones").text(player.notes);
+            }
+            addComment(response);
+        });
+    }
+    
+    function listPlayers() {
+        return $.ajax(playerEndpoint(1), {
+            method: "GET",
+            dataType: "json"
+        });
+    }
+    
+    function reloadPlayers() {
+        listComments().then(function(response) {
+            function addPlayer(player) {
+            	$("#username").text(player.username);
+            }
+//            addPlayer(response);
+//            $("#username").html("");
+            _.forEach(response, addPlayer(response));
+        });
+    }
+    
+    reloadComments();
+    reloadPlayers();
+    
+    
     $("#new_comment").click(function() {
     	$('#myModal').modal('toggle');
     	$('#myModal').modal('show');
+    	
     	$("#myModal #save_comment").click(function() {
-    		var new_comment=$("#myModal #comment").val();
-			$('#myModal').modal('hide');
-			new_comment=$("#myModal #comment").val("");
+    		all=$.ajax(playerEndpoint(1), {
+        		method: "GET",
+        		dataType: "json"
+        	}).then(function(response) {
+        		var new_comment=$("#myModal #comment").val();
+    			response.notes=new_comment;
+        		$('#myModal').modal('hide');
+    			new_comment=$("#myModal #comment").val("");
+    			$("#new_ones").text(response.notes);
+        		console.log(response);
+        		$.ajax(playerEndpoint(1), {
+     			   method: "PUT",
+     			   dataType: "json",
+     			   data: JSON.stringify(response),
+     			   contentType: "application/json; charset=utf-8"
+        		});
+//        		reloadComments();
+        	});
 		});
 	});
+    
     $("#new_task").click(function() {
     	$('#task').modal('toggle');
     	$('#task').modal('show');
@@ -22,7 +82,7 @@ $(document).ready(function() {
     		new_training=$("#task #new_training").val("");
 		});
 	});
-    $("#update_statistic").click(function() {
+    $("#statistic_player").click(function() {
     	$('#statistic').modal('toggle');
     	$('#statistic').modal('show');
     	$("#statistic #update").click(function() {
@@ -43,12 +103,52 @@ $(document).ready(function() {
     	$('#add').modal('show');
     	$("#add #add_player").click(function() {
 			var name=$("#add #name").val();
+			var country=$("#add #country").val();
+			var statistics=$("#add #statistics").val();
+			var fines=$("#add #fines").val();
+			var violations=$("#add #violations").val();
+			var matches=$("#add #matches").val();
+			var notes=$("#add #notes").val();
+			var tasks=$("#add #tasks").val();
+			var results=$("#add #results").val();
+			var diet=$("#add #diet").val();
+			var transfer_value=$("#add #transfer_value").val();
+			var yellow_cards=$("#add #yellow_cards").val();
+			var red_cards=$("#add #red_cards").val();
+			var wage=$("#add #wage").val();
+			var contract=$("#add #contract").val();
 			$('#add').modal('hide');
-			$("#table_body").append("<tr><td>"+name+"</td><td><button type='button' class='btn btn-info' id=comments_player>View comments</button></td>" +
-					"<td><button type='button' class='btn btn-success' id='tasks_player'>View tasks</button></td>" +
-					"<td><button type='button' class='btn btn-warning' id='statistic_player'>View statistic</button></td></tr>");
-    		name=$("#statistic #scored_goals").val("");
-		});
+			$("#table_body").append("<tr><td>"+name+"</td><td><a href='comments.html' role='button' class='btn btn-info' id=comments_player>View comments</a></td>" +
+					"<td><a href='tasks.html' role='button' class='btn btn-success' id='tasks_player'>View tasks</a></td>" +
+					"<td><a href='statistic.html' role='button' class='btn btn-warning' id='statistic_player'>View statistic</a></td></tr>");
+        	var player = {
+    	    	      	username: name,
+    	    	      	country:country,
+    	    	      	statistics:statistics,
+    	    	      	fines:fines,
+    	    	      	violations:violations,
+    	    	      	matches:matches,
+    	    	      	notes:notes,
+    	    	      	tasks:tasks,
+    	    	      	results:results,
+    	    	      	diet:diet,
+    	    	      	transfer_value:transfer_value,
+    	    	      	yellow_cards:yellow_cards,
+    	    	      	red_cards:red_cards,
+    	    	      	wage:wage,
+    	    	      	contract:contract
+    	    	      };
+    	    	      var createPromise = $.ajax(PLAYER_ENDPOINT, {
+    	    	        method: "POST",
+    	    	        contentType: "application/json; charset=utf-8",
+    	    	        data: JSON.stringify(player),
+    	    	        dataType: "json"
+    	    	      }).then(function(response) {
+    	    	        console.log(response);
+    	    	        return response;
+    	    	      });
+			name=$("#statistic #scored_goals").val("");
+    	});
 	});
-    
+   
 });
