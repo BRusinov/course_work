efdfcd2399662265489c68f719e93cda99256a18
BRusinov <borislav.rusinov@gmail.com>
2016-03-09 13:14:12 +0200

    #10 Adding basic functionality js for the tasks page

--- course_work/src/main/webapp/js/script.js
+++ course_work/src/main/webapp/js/script.js
@@ -9,4 +9,18 @@ $(document).ready(function() {
 			new_comment=$("#myModal #comment").val("");
 		});
 	});
+    $("#new_task").click(function() {
+    	$('#task').modal('toggle');
+    	$('#task').modal('show');
+    	$("#task #save_task").click(function() {
+    		var new_matches=$("#task #new_matches").val();
+    		var new_training=$("#task #new_training").val();
+			$('#task').modal('hide');
+			$("#match").append("<p>"+new_matches+"</p>");
+			$("#training").append("<p>"+new_training+"</p>");
+    		new_matches=$("#task #new_matches").val("");
+    		new_training=$("#task #new_training").val("");
+		});
+	});
+    
 });
