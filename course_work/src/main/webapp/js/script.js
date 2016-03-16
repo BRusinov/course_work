8b998a49a6b7b017d1d9bec9d8a0310463525367
BRusinov <borislav.rusinov@gmail.com>
2016-03-16 11:55:29 +0200

    #12 Adding id=coach for the profile page in the index.html. Also removing unnecessary functions and variables from script.js.

--- course_work/src/main/webapp/js/script.js
+++ course_work/src/main/webapp/js/script.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
     "use strict";
     var PLAYER_ENDPOINT = "http://localhost:3000/Regular_user";
-    var COACH_ENDPOINT = "http://localhost:3000/Super_user";
-    
-	function playerEndpoint(playerId) {
-		return PLAYER_ENDPOINT + "/" + playerId;
-	}
-	var all;
-    function listInformation(id) {
-        return $.ajax(playerEndpoint(id), {
-            method: "GET",
-            dataType: "json"
-        });
-    }
-    
     function listAll(){
         return $.ajax(PLAYER_ENDPOINT, {
             method: "GET",
@@ -55,19 +42,6 @@ $(document).ready(function() {
     });
     
     reloadPlayers();
-    $("#new_task").click(function() {
-    	$('#task').modal('toggle');
-    	$('#task').modal('show');
-    	$("#task #save_task").click(function() {
-    		var new_matches=$("#task #new_matches").val();
-    		var new_training=$("#task #new_training").val();
-			$('#task').modal('hide');
-			$("#match").append("<p>"+new_matches+"</p>");
-			$("#training").append("<p>"+new_training+"</p>");
-    		new_matches=$("#task #new_matches").val("");
-    		new_training=$("#task #new_training").val("");
-		});
-	});
     $("#new_player").click(function() {
     	$('#add').modal('toggle');
     	$('#add').modal('show');
