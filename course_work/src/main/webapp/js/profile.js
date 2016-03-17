593002e877f77fae2905d04aeede2532a25a2d70
BRusinov <borislav.rusinov@gmail.com>
2016-03-17 18:05:20 +0200

    #8 Showing coach's information.

--- course_work/src/main/webapp/js/profile.js
+++ course_work/src/main/webapp/js/profile.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
     			$("#coach_name").text(coach.username);
     			$("#coach_country").text(coach.country);
     			$("#coach_age").text(coach.age);
+    			$("#coach_teams").text(coach.teams);
+    			$("#coach_wage").text(coach.wage);
+    			$("#coach_contract").text(coach.contract);
     		}
             $(response).each(function(index, team){
             	showTeam(team);
