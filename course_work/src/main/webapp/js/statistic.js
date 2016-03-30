c9d82fa3d1e4a5e4d5d19b3bc00d1d8ecec20535
BRusinov <borislav.rusinov@gmail.com>
2016-03-30 18:27:17 +0300

    #14 Listing and updating player's information.

--- course_work/src/main/webapp/js/statistic.js
+++ course_work/src/main/webapp/js/statistic.js
@@ -20,10 +20,12 @@ $(document).ready(function() {
             	$("#goals").text(player.statistics.goals);
             	$("#assists").text(player.statistics.assists);
             	$("#minutes").text(player.statistics.minutes);
+            	$("#fines").text(player.fines);
+            	$("#yellow").text(player.yellow_cards);
+            	$("#red").text(player.red_cards);
             	$("#player_name").text(player.username);
             	$("#player_age").text(player.age);
             	$("#player_country").text(player.country);
-            	$("#player_fines").text(player.fines);
             	$("#player_violations").text(player.violations);
             	$("#player_matches").text(player.matches);
             	$("#player_notes").text(player.notes);
@@ -31,8 +33,6 @@ $(document).ready(function() {
             	$("#player_results").text(player.results);
             	$("#player_diet").text(player.diet);
             	$("#player_value").text(player.transfer_value);
-            	$("#player_yellow").text(player.yellow_cards);
-            	$("#player_red").text(player.red_cards);
             	$("#player_wage").text(player.wage);
             	$("#player_contract").text(player.contract);
             }
@@ -53,10 +53,22 @@ $(document).ready(function() {
         		console.log(response);
         		var scored_goals=$("#statistic #scored_goals").val();
         		var made_assists=$("#statistic #made_assists").val();
-        		var minutes_played=$("#statistic #minutes_played").val(); 	   	
+        		var minutes_played=$("#statistic #minutes_played").val(); 
+        		var fines=$("#statistic #fines").val();
+        		var yellow=$("#statistic #yellow").val();
+        		var red=$("#statistic #red").val();
         		var new_goals=parseFloat(scored_goals)+parseFloat(response.statistics.goals);
         		var new_assists=parseFloat(made_assists)+parseFloat(response.statistics.assists)
         		var new_minutes=parseFloat(minutes_played)+parseFloat(response.statistics.minutes);
+        		var new_fines=parseFloat(fines)+parseFloat(response.fines);
+        		var new_yellow=parseFloat(yellow)+parseFloat(response.yellow_cards);
+        		var new_red=parseFloat(red)+parseFloat(response.red_cards);
+        		response.fines=new_fines;
+        		response.yellow_cards=new_yellow;
+        		response.red_cards=new_red;
+        		alert(response.fines);
+        		alert(response.yellow_cards);
+        		alert(response.red_cards);
         		response.statistics={
         				goals:new_goals,
         				assists:new_assists,
@@ -72,6 +84,9 @@ $(document).ready(function() {
     			$("#goals").text(response.statistics.goals);
     			$("#assists").text(response.statistics.assists);
     			$("#minutes").text(response.statistics.minutes);
+        		scored_goals=$("#statistic #scored_goals").val("");
+        		made_assists=$("#statistic #made_assists").val("");
+        		minutes_played=$("#statistic #minutes_played").val("");
         		scored_goals=$("#statistic #scored_goals").val("");
         		made_assists=$("#statistic #made_assists").val("");
         		minutes_played=$("#statistic #minutes_played").val("");
